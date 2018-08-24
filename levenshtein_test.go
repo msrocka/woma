8567cefc87eb141af953fdc4945be53227bacf8d
msrocka <michael.srocka@gmail.com>
2018-08-24 19:57:13 +0200

    Levenshtein tests

--- levenshtein_test.go
+++ levenshtein_test.go
@@ -5,10 +5,22 @@ import (
 )
 
 func TestLevenshtein(t *testing.T) {
+	if Levenshtein("", "") != 0 {
+		t.Fatal("lev(,) != 0")
+	}
+	if Levenshtein("über", "über") != 0 {
+		t.Fatal("lev(,) != 0")
+	}
 	if Levenshtein("acb", "adb") != 1 {
 		t.Fatal("lev(acb, adb) != 1")
 	}
-	if Levenshtein("über", "übäer") != 2 {
-		t.Fatal("lev(über, übäer) != 2")
+	if Levenshtein("über", "übäer") != 1 {
+		t.Fatal("lev(über, übäer) != 1")
+	}
+	if Levenshtein("über", "übäeer") != 2 {
+		t.Fatal("lev(über, übäeer) != 2")
+	}
+	if Levenshtein("trüber", "träbäeer") != 3 {
+		t.Fatal("lev(trüber, träbäeer) != 3")
 	}
 }
