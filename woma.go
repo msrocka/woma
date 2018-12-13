8e3ca4e2633b511a4c6b7a06f9d45605ebe8ff0a
msrocka <michael.srocka@gmail.com>
2018-12-13 09:24:24 +0100

    Dice coefficients and n-grams as composable functions ...

--- woma.go
+++ woma.go
@@ -10,24 +10,64 @@ func Normalize(s string) string {
 	return strings.ToLower(strings.TrimSpace(s))
 }
 
-func Ngrams(word string) []string {
+// Affixes returns the set of prefixes and suffixes of the given word.
+func Affixes(word string) map[string]bool {
 	nword := Normalize(word)
-	var ngrams []string
+	affixes := make(map[string]bool)
 	if nword == "" {
-		return ngrams
+		return affixes
 	}
 	runes := []rune(nword)
 	if len(runes) < 2 {
-		return ngrams
+		return affixes
 	}
 	for i := range runes {
 		if i == 0 {
 			continue
 		}
 		if i < (len(runes)) {
-			ngrams = append(ngrams, string(runes[0:i]))
+			affixes[string(runes[0:i])] = true
 		}
-		ngrams = append(ngrams, string(runes[i:]))
+		affixes[string(runes[i:])] = true
 	}
-	return ngrams
+	return affixes
+}
+
+// Ngrams constructs a function that calculates the n-grams of a word.
+func Ngrams(n int) func(string) map[string]bool {
+	return func(word string) map[string]bool {
+		ngrams := make(map[string]bool)
+		nword := Normalize(word)
+		if len(nword) < n {
+			return ngrams
+		}
+		runes := []rune(nword)
+		for i := range runes {
+			ngram := string(runes[i:(i + n)])
+			ngrams[ngram] = true
+			if (i + n) == len(runes) {
+				break
+			}
+		}
+		return ngrams
+	}
+}
+
+// Dice returns a function that calculates the Dice coefficient
+// https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient of
+// two words: 2 * |A & B| / (|A| + |B|)
+// It takes a function as argument that calculates the sets A and B from the
+// given words.
+func Dice(fn func(string) map[string]bool) func(string, string) float64 {
+	return func(wordA, wordB string) float64 {
+		a := fn(wordA)
+		b := fn(wordB)
+		u := 0
+		for e := range a {
+			if b[e] {
+				u++
+			}
+		}
+		return float64(2*u) / float64(len(a)+len(b))
+	}
 }
--- woma_test.go
+++ woma_test.go
@@ -1,27 +1,54 @@
 package woma
 
 import (
+	"math"
 	"testing"
 )
 
-func TestNgrams(t *testing.T) {
-	if len(Ngrams("")) != 0 {
-		t.Fatal("The empty word should have no n-grams")
+func TestAffixes(t *testing.T) {
+	if len(Affixes("")) != 0 {
+		t.Fatal("The empty word should have no affixes")
 	}
-	ngrams := Ngrams("works")
-	if len(ngrams) != 8 {
-		t.Fatal("Should get 8 n-grams from 'works'")
+	affixes := Affixes("works")
+	if len(affixes) != 8 {
+		t.Fatal("Should get 8 affixes from 'works'")
 	}
 	expected := []string{"w", "wo", "wor", "work",
 		"orks", "rks", "ks", "s"}
 	for _, e := range expected {
-		for i, ng := range ngrams {
-			if e == ng {
-				break
-			}
-			if i == (len(ngrams) - 1) {
-				t.Fatal("Could not find", e)
-			}
+		if !affixes[e] {
+			t.Fatal("Could not find", e)
 		}
+	}
+}
+
+func TestNgrams(t *testing.T) {
+	if len(Ngrams(2)("")) != 0 {
+		t.Fatal("The empty word should have no n-grams")
+	}
+	bigrams := Ngrams(2)("night")
+	if len(bigrams) != 4 {
+		t.Fatal("Should get 4 bigrams from 'night'")
+	}
+	expected := []string{"ni", "ig", "gh", "ht"}
+	for _, e := range expected {
+		if !bigrams[e] {
+			t.Fatal("Could not find", e)
+		}
+	}
+}
+
+func TestDice(t *testing.T) {
+	dsc := Dice(Ngrams(2))("night", "nacht")
+	if math.Abs(dsc-0.25) > 1e-16 {
+		t.Fatal("Should get a Dice score of 0.25")
+	}
+	dsc = Dice(Affixes)("work", "wirk")
+	if math.Abs(dsc-0.5) > 1e-16 {
+		t.Fatal("Should get a Dice score of 0.5")
+	}
+	dsc = Dice(Affixes)("", "wirk")
+	if math.Abs(dsc) > 1e-16 {
+		t.Fatal("Should get a Dice score of 0")
 	}
 }
